53aeb53f95c1425ff52cba9a59e700d7effb567d
Kyle Pak <pak.kyle@hotmail.com>
2019-04-12 16:33:04 -0700

    Updated fix for double result when it should be int. Swapped location for - and * buttons.

--- app/src/main/java/com/example/kpcalculator/MainActivity.java
+++ app/src/main/java/com/example/kpcalculator/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.lang.Math.*;
+
+import static java.lang.Math.round;
+import static java.lang.Math.toIntExact;
 
 public class MainActivity extends AppCompatActivity {
     Button button1, button2, button3, button4, button5, button6, button7, button8, button9,
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         buttonDiv = (Button) findViewById(R.id.button17);
         txtView = (TextView) findViewById(R.id.textView);
 
+        //Display number on text view
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,7 +104,20 @@ public class MainActivity extends AppCompatActivity {
         buttonDec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtView.setText(txtView.getText() + ".");
+                String decimal = txtView.getText().toString();
+                //Checks to see if there is a decimal already in textview
+                boolean hasDec = false;
+                for (int i = 0; i < decimal.length(); i++){
+                    char c = decimal.charAt(i);
+                    if (c == '.'){
+                        hasDec = true;
+                    }
+                }
+                if (hasDec == true){
+                    txtView.setText(txtView.getText());
+                } else{
+                    txtView.setText(txtView.getText() + ".");
+                }
             }
         });
         buttonClr.setOnClickListener(new View.OnClickListener() {
@@ -110,66 +128,127 @@ public class MainActivity extends AppCompatActivity {
                 numTwo = null;
             }
         });
+
+        /*Checks to see if a number is not stored for operation
+         */
         if (numOne == null) {
+
+            //Addition operation
             buttonAdd.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     numOne = txtView.getText();
                     txtView.setText("");
-                    buttonEq.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            numTwo = txtView.getText();
-                            Double result = Double.parseDouble(numOne.toString()) + Double.parseDouble(numTwo.toString());
-                            txtView.setText(Double.toString(result));
-                        }
-                    });
-
+                    if (numOne != null) {
+                        buttonEq.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                numTwo = txtView.getText();
+                                Double one = Double.parseDouble(numOne.toString());
+                                Double two = Double.parseDouble(numTwo.toString());
+                                if ((one%1) != 0 || (two%1) != 0) {
+                                    Double result = one + two;
+                                    txtView.setText(Double.toString(result));
+                                } else {
+                                    int result = toIntExact(round(one) + round(two));
+                                    txtView.setText(Integer.toString(result));
+                                }
+                            }
+                        });
+                    }
                 }
             });
+
+            //Subtraction operation
             buttonSub.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     numOne = txtView.getText();
                     txtView.setText("");
-                    buttonEq.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            numTwo = txtView.getText();
-                            Double result = Double.parseDouble(numOne.toString()) - Double.parseDouble(numTwo.toString());
-                            txtView.setText(Double.toString(result));
-                        }
-                    });
+                    if (numOne != null) {
+                        buttonEq.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                numTwo = txtView.getText();
+                                Double one = Double.parseDouble(numOne.toString());
+                                Double two = Double.parseDouble(numTwo.toString());
+                                if ((one%1) != 0 || (two%1) != 0) {
+                                    Double result = one - two;
+                                    txtView.setText(Double.toString(result));
+                                } else {
+                                    int result = toIntExact(round(one) - round(two));
+                                    txtView.setText(Integer.toString(result));
+                                }
+                            }
+                        });
+                    }
                 }
             });
+
+            //Multiplication operation
             buttonMult.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v){
                     numOne = txtView.getText();
                     txtView.setText("");
-                    buttonEq.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            numTwo = txtView.getText();
-                            Double result = Double.parseDouble(numOne.toString()) * Double.parseDouble(numTwo.toString());
-                            txtView.setText(Double.toString(result));
-                        }
-                    });
+                    if (numOne != null) {
+                        buttonEq.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                numTwo = txtView.getText();
+                                Double one = Double.parseDouble(numOne.toString());
+                                Double two = Double.parseDouble(numTwo.toString());
+                                if ((one%1) != 0 || (two%1) != 0) {
+                                    Double result = one * two;
+                                    txtView.setText(Double.toString(result));
+                                } else {
+                                    int result = toIntExact(round(one) * round(two));
+                                    txtView.setText(Integer.toString(result));
+                                }
+                            }
+                        });
+                    }
                 }
             });
+
+            //Division operation
             buttonDiv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v){
                     numOne = txtView.getText();
                     txtView.setText("");
-                    buttonEq.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            numTwo = txtView.getText();
-                            Double result = Double.parseDouble(numOne.toString()) / Double.parseDouble(numTwo.toString());
-                            txtView.setText(Double.toString(result));
-                        }
-                    });
+                    if (numOne != null) {
+                        buttonEq.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                numTwo = txtView.getText();
+                                Double one = Double.parseDouble(numOne.toString());
+                                Double two = Double.parseDouble(numTwo.toString());
+                                Double result = one / two;
+                                if ((result%1) != 0) {
+                                    txtView.setText(Double.toString(result));
+                                } else {
+                                    int intResult = toIntExact(round(result));
+                                    txtView.setText(Integer.toString(intResult));
+                                }
+                            }
+                        });
+                    }
                 }
             });
-        }
+        }// end of if
     }
-
-
 }
+//public class addNum(){
+//    buttonAdd.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            var numOne = txtView.getText();
+//            txtView.setText("");
+//            buttonEq.setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View v) {
+//                    numTwo = txtView.getText();
+//                    Double result = Double.parseDouble(numOne.toString()) + Double.parseDouble(numTwo.toString());
+//                    txtView.setText(Double.toString(result));
+//                }
+//            });
+//
+//        }
+//    });
+//}
